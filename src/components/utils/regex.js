2fa73aeb8e55a03139fd1e821fd0ff62cb048ab9
thehoap <thehoap.2000@gmail.com>
2022-06-25 00:17:52 +0700

    init regex

---
+++ src/components/utils/regex.js
@@ -0,0 +1,7 @@
+export const REGEX_NAME =
+    /^[^\s0-9$&+,:;=?@#|'<>.\\\-_^*()%!][aAàÀảẢãÃáÁạẠăĂằẰẳẲẵẴắẮặẶâÂầẦẩẨẫẪấẤậẬbBcCdDđĐeEèÈẻẺẽẼéÉẹẸêÊềỀểỂễỄếẾệỆfFgGhHiIìÌỉỈĩĨíÍịỊjJkKlLmMnNoOòÒỏỎõÕóÓọỌôÔồỒổỔỗỖốỐộỘơƠờỜởỞỡỠớỚợỢpPqQrRsStTuUùÙủỦũŨúÚụỤưƯừỪửỬữỮứỨựỰvVwWxXyYỳỲỷỶỹỸýÝỵỴzZ,\s]+[^\s0-9$&+,:;=?@#|'<>.\\\-_^*()%!]$/;
+export const REGEX_EMAIL = /^[a-z0-9](\.?[a-z0-9]){5,}@gmail\.com$/;
+export const REGEX_PHONE = /(84|0[3|5|7|8|9])+([0-9]{8})\b/;
+export const REGEX_NUMBER_ONLY = /^[0-9]*$/;
+export const REGEX_ADDRESS =
+    /^[^\s$&+,:;=?@#|'<>.\\\-_^*()%!][0-9aAàÀảẢãÃáÁạẠăĂằẰẳẲẵẴắẮặẶâÂầẦẩẨẫẪấẤậẬbBcCdDđĐeEèÈẻẺẽẼéÉẹẸêÊềỀểỂễỄếẾệỆfFgGhHiIìÌỉỈĩĨíÍịỊjJkKlLmMnNoOòÒỏỎõÕóÓọỌôÔồỒổỔỗỖốỐộỘơƠờỜởỞỡỠớỚợỢpPqQrRsStTuUùÙủỦũŨúÚụỤưƯừỪửỬữỮứỨựỰvVwWxXyYỳỲỷỶỹỸýÝỵỴzZ,\s]+[^\s0-9$&+,:;=?@#|'<>.\\\-_^*()%!]$/;
